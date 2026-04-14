c731a296fb3913ed13d9fb08734a161ecc853cfd
nperreault27 <nick.d.perreault@gmail.com>
2026-04-14 15:08:32 -0400

    added create event and showevent createform

---
+++ src/controller/EventController.ts
@@ -0,0 +1,18 @@
+import { IEventService } from "../service/EventService";
+
+export interface IEventController {
+    createEvent(res: Response, content: string): void;
+    showEventCreateForm(res: Response): void;
+}
+
+class EventController implements IEventController {
+    constructor(private readonly eventService: IEventService) {
+    }
+    createEvent(res: Response, content: string) {
+        const { title, description, location, category, status, capacity, startDatetime, endDatetime, organizerId } = JSON.parse(content);
+        this.eventService.createEvent(title, description, location, category, status, capacity, new Date(startDatetime), new Date(endDatetime), organizerId);
+    }
+    showEventCreateForm() {
+        console.log("Displaying event creation form...");
+    }
+}
